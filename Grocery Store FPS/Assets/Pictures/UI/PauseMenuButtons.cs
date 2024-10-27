164c946af3d3074de13cc1857d6cc6e9564e1827
NtsikeleloMadlongolwane <2154536@students.wits.ac.za>
2024-10-27 09:58:25 +0200

    In game menus done (buttons need to be coded though)

---
+++ Grocery Store FPS/Assets/Pictures/UI/PauseMenuButtons.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuButtons : MonoBehaviour
+{
+    public Vector3[] positions = new Vector3[4];
+    public Image[] potions = new Image[4];
+    public Button moveLeftButton;
+    public Button moveRightButton;
+    public Button[] potionButtons = new Button[4];
+    public int centerOption = 1;
+
+    private int[] indices = new int[4];
+
+    void Start()
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            indices[i] = i;
+        }
+        moveLeftButton.onClick.AddListener(MoveLeft);
+        moveRightButton.onClick.AddListener(MoveRight);
+        SetPotionPositions();
+        LogCurrentPotion();
+
+        // Set up button interactions
+        for (int i = 0; i < potionButtons.Length; i++)
+        {
+            int index = i;
+            potionButtons[i].onClick.AddListener(() => OnPotionButtonClick(index));
+        }
+    }
+
+    void SetPotionPositions()
+    {
+        for (int i = 0; i < potions.Length; i++)
+        {
+            potions[indices[i]].rectTransform.localPosition = positions[i];
+            potionButtons[indices[i]].GetComponent<RectTransform>().localPosition = positions[i]; // Correctly get RectTransform
+            potions[indices[i]].rectTransform.localScale = (i == centerOption)
+                ? new Vector3(1f, 1f, 1f)
+                : new Vector3(0.5f, 0.5f, 1f);
+
+            // Enable or disable buttons based on their position
+            potionButtons[indices[i]].gameObject.SetActive(i == centerOption);
+        }
+    }
+
+    void MoveLeft()
+    {
+        int tempIndex = indices[0];
+        for (int i = 0; i < indices.Length - 1; i++)
+        {
+            indices[i] = indices[i + 1];
+        }
+        indices[indices.Length - 1] = tempIndex;
+        SetPotionPositions();
+        LogCurrentPotion();
+    }
+
+    void MoveRight()
+    {
+        int tempIndex = indices[indices.Length - 1];
+        for (int i = indices.Length - 1; i > 0; i--)
+        {
+            indices[i] = indices[i - 1];
+        }
+        indices[0] = tempIndex;
+        SetPotionPositions();
+        LogCurrentPotion();
+    }
+
+    void LogCurrentPotion()
+    {
+        Debug.Log("Image in third position: " + potions[indices[1]].name);
+    }
+
+    void OnPotionButtonClick(int index)
+    {
+        Debug.Log("Potion button clicked: " + potionButtons[index].name);
+    }
+}
